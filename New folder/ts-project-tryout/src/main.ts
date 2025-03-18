3e1e40ceaa67b5174d7abb88ecc627bc94ec726e
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-18 11:49:22 +0400

    sent email

--- New folder/ts-project-tryout/src/main.ts
+++ New folder/ts-project-tryout/src/main.ts
@@ -8,7 +8,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <h1>Email Form</h1>
     <div class="card">
       <input class="email-from email-form-input " placeholder="From"/>
-      <input class="email-header email-form-input" placeholder="Header"/>
       <input class="email-subject email-form-input" placeholder="Subject"/>
       <textarea class="email-text email-form-input" placeholder="text"> </textarea>
 
@@ -22,31 +21,34 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 const bodyElement = document.body as HTMLBodyElement;
 const emailFromElement = document.querySelector<HTMLInputElement>(".email-from")
-const emailHeaderElement = document.querySelector<HTMLInputElement>(".email-header")
 const emailSubjectElement = document.querySelector<HTMLInputElement>(".email-subject")
 const emailTextElement = document.querySelector<HTMLInputElement>(".email-text")
 
 bodyElement.addEventListener("keypress", (e) => {
   if(e.key === "Enter") {
 
-    if(emailFromElement && emailHeaderElement && emailSubjectElement && emailTextElement) {
+    if(emailFromElement  && emailSubjectElement && emailTextElement) {
       let emailFrom = emailFromElement.value
-      let emailHeader = emailHeaderElement.value
       let emailSubject = emailSubjectElement.value
       let emailText = emailTextElement.value
-      handleSubmit(emailFrom, emailHeader, emailSubject, emailText)
+      handleSubmit(emailFrom, emailSubject, emailText)
     }
   }
 })
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function resetInputValues() {
+  if(emailFromElement  && emailSubjectElement && emailTextElement) {
+    emailFromElement.value = ""
+    emailSubjectElement.value = ""
+    emailTextElement.value = ""
+  }
+}
 
-
-async function handleSubmit(from: string, header:string, subject: string, text: string) {
+async function handleSubmit(from: string, subject: string, text: string) {
   try {
     console.log(`
       Email Sent By ${from}
-      Email Header: ${header}
       Email subject: ${subject}
       ${text}
       `)
