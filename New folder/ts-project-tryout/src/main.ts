be18d50808f3bc66e093641539c12232cfac8e30
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-18 11:05:24 +0400

    startup

---
+++ New folder/ts-project-tryout/src/main.ts
@@ -0,0 +1,18 @@
+import './style.css'
+import emailLogo from '../public/email.png'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <img src="${emailLogo}" class="logo" alt="Vite logo" />
+    <h1>Email Form</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
