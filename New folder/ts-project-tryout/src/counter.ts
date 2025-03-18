6d9570608534836be3e45ee9abe63eb1c9b85c5b
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-03-18 11:45:42 +0400

    sent email

--- New folder/ts-project-tryout/src/counter.ts
+++ New folder/ts-project-tryout/src/counter.ts
@@ -8,4 +8,5 @@ export function setupCounter(element: HTMLButtonElement) {
   setCounter(0)
 }
 
+
 console.log("HELLO")
--- New folder/ts-project-tryout/src/main.ts
+++ New folder/ts-project-tryout/src/main.ts
@@ -1,13 +1,18 @@
 import './style.css'
 import emailLogo from '../public/email.png'
-import { setupCounter } from './counter.ts'
+// import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <img src="${emailLogo}" class="logo" alt="Vite logo" />
     <h1>Email Form</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <input class="email-from email-form-input " placeholder="From"/>
+      <input class="email-header email-form-input" placeholder="Header"/>
+      <input class="email-subject email-form-input" placeholder="Subject"/>
+      <textarea class="email-text email-form-input" placeholder="text"> </textarea>
+
+      <button id="counter" type="button">Send Email</button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
@@ -15,4 +20,40 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const bodyElement = document.body as HTMLBodyElement;
+const emailFromElement = document.querySelector<HTMLInputElement>(".email-from")
+const emailHeaderElement = document.querySelector<HTMLInputElement>(".email-header")
+const emailSubjectElement = document.querySelector<HTMLInputElement>(".email-subject")
+const emailTextElement = document.querySelector<HTMLInputElement>(".email-text")
+
+bodyElement.addEventListener("keypress", (e) => {
+  if(e.key === "Enter") {
+
+    if(emailFromElement && emailHeaderElement && emailSubjectElement && emailTextElement) {
+      let emailFrom = emailFromElement.value
+      let emailHeader = emailHeaderElement.value
+      let emailSubject = emailSubjectElement.value
+      let emailText = emailTextElement.value
+      handleSubmit(emailFrom, emailHeader, emailSubject, emailText)
+    }
+  }
+})
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+
+async function handleSubmit(from: string, header:string, subject: string, text: string) {
+  try {
+    console.log(`
+      Email Sent By ${from}
+      Email Header: ${header}
+      Email subject: ${subject}
+      ${text}
+      `)
+  } catch(err) {
+    console.log("Err: ", err)
+  } finally {
+    resetInputValues()
+  }
+  
+}
